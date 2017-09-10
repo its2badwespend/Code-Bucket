fd3b5e87eb1570d6719a9c3a0df931f76e75c209
Joe Williamson <jhwilliamson@outlook.com>
2017-09-10 16:16:20 -0400

    Panel toggle fx

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-$(document).ready(function() {
+$( document ).ready(function() {
+    console.log( "ready!" );
     
+$( "#html-button" ).click(function() {
+  $( "#html-box" ).toggle('slow');
+});  
+
+$( "#css-button" ).click(function() {
+  $( "#css-box" ).toggle('slow');
+});    
     
+$( "#js-button" ).click(function() {
+  $( "#js-box" ).toggle('slow');
+});    
     
-    
-    
+$( "#output-button" ).click(function() {
+  $( "#output-box" ).toggle('slow');
+});    
     
 });
