4164982ae32bd0b586bb0327de5290d52a77083f
Joe Williamson <jhwilliamson@outlook.com>
2017-09-10 19:06:17 -0400

    iframe wired up

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$( document ).ready(() => {
     console.log( "ready!" );
     
 $( "#html-button" ).click(function() {
@@ -16,5 +16,20 @@ $( "#js-button" ).click(function() {
 $( "#output-button" ).click(function() {
   $( "#output-box" ).toggle('slow');
 });    
+
+let newsrc = `<!DOCTYPE html>\n<html>\n<head>\n<meta charset='utf-8' />\n<style>\n ${$("#css-box").value}\n</style>\n</head>\n<body>\n ${$("#html-box").value}\n<script>\n ${$("#js-box").value}\n<\/script>\n<\/body>\n<\/html>`;
+
+let iframe = $('#output-box');
+
+setInterval(() => {
+     iframe.srcdoc = $('#html-box').value + '<style>' +
+                $('#css-box').value + '</style>' + '<script>' +
+                $('#js-box').value + '<\/script>'}, 1000)
+
+
+//setInterval(() => {
+//        $('#output-box').attr('src', 'https://google.com');
+//        }, 5000);
     
 });
+
